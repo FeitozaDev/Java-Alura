827ccea5dcfc928dc62643d22615520c072a2421
Lucas Feitoza <lucassilva202414@outlook.com>
2026-01-08 18:24:53 -0300

    Initial commit

---
+++ src/ExerciciosDeFixação.java
@@ -0,0 +1,68 @@
+// Exercicios:
+
+
+
+public class ExercicioTemperatura {
+    public static void main (String [] args ){
+
+
+        // Converter temperatura de Celsius para Fahrenheit.
+
+        /* double temperaturaCelsius = 35.4;
+        double temperaturaFahrenheit = (temperaturaCelsius * 1.8) + 32;
+
+        String mensagem = String.format("A temperatura de %f Celsius é equivalente a %f Fahrenheit", temperaturaCelsius, temperaturaFahrenheit);
+
+        System.out.println(mensagem);
+
+        int temperaturaFahrenheitInteira = (int) temperaturaFahrenheit;
+        System.out.println("A temperatura em Fahrenheit inteira é de: " + temperaturaFahrenheitInteira); */
+
+
+
+        // Crie um programa que realize a média de duas notas decimais e exiba o resultado. :
+
+       /*  float nota1 = 8.5f;
+        float nota2 = 6.5f;
+
+        float media = (nota1 + nota2) / 2;
+
+        System.out.println( "A média das duas notas é : " + media); */
+
+
+        // Declare uma variável do tipo double e uma variável do tipo int. Faça o casting da variável double para int e imprima o resultado.
+
+        /*double variavel1 = 8.7;
+        int variavel2 = (int) variavel1;
+
+        System.out.println("Casting é : " + variavel2); */
+
+
+        // Declare uma variável do tipo char (letra) e uma variável do tipo String (palavra). Atribua valores a essas variáveis e concatene-as em uma mensagem.
+
+        /*
+        char letra = 'H';
+
+        String palavra = "Hoje o dia esta bonito.";
+
+        System.out.println("A palavra é " + palavra + " e a Letra é : " + letra);*/
+
+
+        // Declare uma variável do tipo double precoProduto e uma variável do tipo int (quantidade). Calcule o valor total multiplicando o preço do produto pela quantidade e apresente o resultado em uma mensagem.
+
+
+        /*
+        double precoProduto = 59.90;
+
+        int quantidade = 10;
+
+        double total = precoProduto * quantidade;
+
+        String mensagem = "O valor em Reais dos produtos em estoque é de : " + total;
+        System.out.println(mensagem); */
+
+        
+
+
+    }
+}
---
+++ src/Perfil.java
@@ -0,0 +1,9 @@
+public class Perfil {
+    public static void main(String[] args) {
+        String nome = "Lucas Feitoza";
+        int idade = 26;
+        System.out.println("Olá meu nome é " + nome + " e tenho " + idade + " anos, tudo bem? ");
+
+    }
+
+}
